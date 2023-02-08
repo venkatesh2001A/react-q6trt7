a1b67d3ba1b93933fdd4222722ea188366e5367c
venkatesh2001A <121805934+venkatesh2001A@users.noreply.github.com>
2023-02-08 19:00:41 +0530

    employee list

--- src/pages/employeelist.jsx
+++ src/pages/employeelist.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import '../css/employeelist.css';
 import profile from '../assets/profile.jpg';
+
+const data = [];
 function Employeelist() {
-  return (
-    <div className="card">
-      <img
-        src="https://www.olympus-imaging.co.in/content/000107507.jpg"
-        alt="employee"
-        // style="width:100%"
-      />
-      <div className="container">
-        <h4>
-          <b>venkatesh</b>
-        </h4>
-        <p>18cs101</p>
-        <button>view</button>
+  const SingleEmployee = (e) => {
+    return (
+      <div className="card">
+        <img
+          src="https://www.olympus-imaging.co.in/content/000107507.jpg"
+          alt="employee"
+          // style="width:100%"
+        />
+        <div className="container">
+          <h4>
+            <b>venkatesh</b>
+          </h4>
+          <p>18cs101</p>
+          <button>view</button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
+//   const allEmployee = () => {
+//     return data.map((e) => <div className="card">{singleEmployee(e)}</div>);
+//   };
+//   return <div>{allEmployee()}</div>;
+ }
 export default Employeelist;
