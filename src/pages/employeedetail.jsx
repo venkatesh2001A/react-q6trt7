b6ebf1e5e726e6daf1adafb0b3e10fd286da38a5
venkatesh2001A <121805934+venkatesh2001A@users.noreply.github.com>
2023-02-17 19:34:35 +0530

    1 file modified

--- src/pages/employeedetail.jsx
+++ src/pages/employeedetail.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import '../css/employeedetail.css';
-function  Detail() {
-  return (
-    <div className="demo">
-        <div className="card"></div>
-            <img src={demo} alt="demo" style="width:90%">
+// import React from 'react';
+// import '../css/employeedetail.css';
+// function  Detail() {
+//   return (
+//     <div className="demo">
+//         <div className="card"></div>
+//             <img src={demo} alt="demo" style="width:90%">
 
-  )
-}
+//   )
+// }
