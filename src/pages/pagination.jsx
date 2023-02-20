09aca3bf06a6ca75d030cf2ce0f20b4cef88a86b
venkatesh2001A <121805934+venkatesh2001A@users.noreply.github.com>
2023-02-20 18:59:45 +0530

    1 file modified

--- src/pages/pagination.jsx
+++ src/pages/pagination.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
-
+import '../css/pagination.css';
 function Pagination() {
-  return <p>venkatesh</p>;
-}
+  const [data, setdata] = React.useState([]);
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setdata(result);
+      });
+  }, []);
 
+  const generateHead = () => {
+    const keys = Object.keys(data[0]);
+    console.log(keys);
+    // return <th>hello</th>;
+
+    return keys.map((e) => <th>{e}</th>);
+  };
+  const generaterow = () => {
+    return data.map((row) => <tr>"hello"</tr>);
+  };
+  return (
+    // <p> PUGZHAL {JSON.stringify(data)}</p>
+    <div>
+      <table>
+        <thead>
+          <tr> {data.length > 0 ? generateHead() : 'loading...'} </tr>
+        </thead>
+        <tbody>{generaterow()}</tbody>
+      </table>
+    </div>
+  );
+}
 export default Pagination;
