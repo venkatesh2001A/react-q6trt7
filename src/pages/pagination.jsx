d15727e4d9e19e4843d6e7f9d3a7d6e756c6491d
venkatesh2001A <121805934+venkatesh2001A@users.noreply.github.com>
2023-02-21 10:47:34 +0530

    1 file modified, 1 added

--- src/pages/pagination.jsx
+++ src/pages/pagination.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../css/pagination.css';
 function Pagination() {
   const [data, setdata] = React.useState([]);
+  const [pagelimit, setpagelimit] = React.useState(10);
+  const [page, setpage] = React.useState(1);
+
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/comments')
       .then((res) => res.json())
@@ -18,17 +21,36 @@ function Pagination() {
 
     return keys.map((e) => <th>{e}</th>);
   };
-  const generaterow = () => {
-    return data.map((row) => <tr>"hello"</tr>);
+
+  const generatedata = (row) => {
+    const keys = Object.keys(data[0]);
+    console.log(keys);
+    return keys.map((e) => <td>{row[e]}</td>);
   };
+
+  const generaterow = (page) => {
+    const start = (page - 1) * pagelimit;
+    const end = start + pagelimit;
+    return data.slice(start, end).map((row) => <tr>{generatedata(row)}</tr>);
+  };
+
+  const generatebuttons = () => {
+    const lastpage = Math.ceil(data.length / pagelimit);
+    const buttons = [];
+    for (let i = 1; i <= lastpage; i++) buttons.push(i);
+    return buttons.map((eachbutton) => (
+      <button onClick={() => setpage(eachbutton)}>{eachbutton}</button>
+    ));
+  };
+
   return (
-    // <p> PUGZHAL {JSON.stringify(data)}</p>
     <div>
+      <div>{generatebuttons()}</div>
       <table>
         <thead>
           <tr> {data.length > 0 ? generateHead() : 'loading...'} </tr>
         </thead>
-        <tbody>{generaterow()}</tbody>
+        <tbody>{generaterow(page)}</tbody>
       </table>
     </div>
   );
